c7b3455b68ee0bcf58a620264447391a2529f85d
Yuki Teraoka <y.teraoka8@gmail.com>
2018-12-01 20:43:38 +0900

    Update: module

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,30 @@
 import buble from 'rollup-plugin-buble'
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 
-export default {
-  input: 'src/mediancut.js',
-  output: {
-    file: 'dist/mediancut.js',
-    format: 'umd',
-    name: 'MedianCut',
+export default [
+  {
+    input: 'src/mediancut.js',
+    output: {
+      file: 'dist/mediancut.js',
+      format: 'umd',
+      name: 'MedianCut',
+    },
+    plugins: plugins(),
   },
-  plugins: [
+  {
+    input: 'src/mediancut.js',
+    output: {
+      file: 'docs/mediancut.js',
+      format: 'umd',
+      name: 'MedianCut',
+    },
+    plugins: plugins(),
+  }
+]
+
+function plugins(){
+  return [
     buble(),
     uglify()
-  ],
+  ];
 }
