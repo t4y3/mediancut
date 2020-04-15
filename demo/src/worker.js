d918a0e19bb0371687d6d082990643f35fbed156
t4y3 <y.teraoka8@gmail.com>
2020-04-15 22:15:07 +0900

    Update: demo

--- demo/src/worker.js
+++ demo/src/worker.js
@@ -3,5 +3,8 @@ importScripts('https://unpkg.com/mediancut@latest');
 self.addEventListener('message', ({data}) => {
   const medianCut = new MedianCut(data.imageData);
   const imageData = medianCut.reduce(data.size);
+
+  console.warn(111111, medianCut.colors);
+
   self.postMessage(imageData, [imageData.data.buffer]);
 });
